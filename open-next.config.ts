c7225cee77cfd070d634d8aab5b38c4c7c6af6ed
Jesus Cadena <jcdevs53@gmail.com>
2026-09-07 12:30:01 -0700

    open next fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,16 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-export default defineCloudflareConfig({
+const cloudflareConfig = defineCloudflareConfig({
 	// Uncomment to enable R2 cache,
 	// It should be imported as:
 	// `import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";`
 	// See https://opennext.js.org/cloudflare/caching for more details
 	// incrementalCache: r2IncrementalCache,
 });
+
+export default {
+	...cloudflareConfig,
+	// Prevent the adapter from recursively invoking the app's `npm run build`
+	// while it is already performing the Next.js build.
+	buildCommand: "next build",
+};
